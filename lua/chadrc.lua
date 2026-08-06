0f31c356260111393ca4b38155020369a5469a84
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-08-06 21:58:45 +0700

    change statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,27 +6,41 @@
 local M = {}
 M.lsp = {
   signature = {
-    enabled = false,
+    enabled = true,
   },
 }
 M.base46 = {
-  theme = "seoul256_dark",
+  theme = "rosepine",
   transparency = true,
   hl_override = {
-    Comment = { italic = true },
+    Comment = { italic = false },
     ["@comment"] = { italic = true },
   },
 }
 
-M.nvdash = { load_on_startup = true }
+M.nvdash = { load_on_startup = false }
 M.ui = {
   statusline = {
-    enabled = true,
     theme = "vscode", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
-    separator_style = "default", -- default/round/block/arrow
-    order = nil,
+    separator_style = "default",
+    order = {
+      "mode",
+      "file",
+      "git",
+      "%=",
+      "lsp_msg",
+      "lsp",
+      -- "python_venv",
+      -- "diagnostics",
+      -- "debug_status",
+      -- "command",
+      -- "lazy_updates",
+      -- "clients",
+      -- "cwd",
+      -- "total_lines",
+    },
     modules = nil,
   },
 
