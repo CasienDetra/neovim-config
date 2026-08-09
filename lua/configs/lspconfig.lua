098fbdb756da9d9d7416db9b4eb1680e008fe0ec
ArtemisCasien <nuk385916@gmail.com>
2026-08-09 14:04:16 +0700

    sd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -13,10 +13,19 @@ local servers = {
 }
 vim.lsp.enable(servers)
 -- Diagnostic UI setup
+-- Diagnostic floating window
 vim.diagnostic.config {
   severity_sort = true,
-  float = { border = "rounded", source = "if_many" },
-  underline = { severity = vim.diagnostic.severity.ERROR },
+
+  float = {
+    border = "rounded",
+    source = "if_many",
+  },
+
+  underline = {
+    severity = vim.diagnostic.severity.ERROR,
+  },
+
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = "󰅚 ",
@@ -25,12 +34,21 @@ vim.diagnostic.config {
       [vim.diagnostic.severity.HINT] = "󰌶 ",
     },
   },
+
   virtual_text = {
     source = "if_many",
     spacing = 2,
+
     format = function(diagnostic)
       return diagnostic.message:match "^[^\n]+"
     end,
   },
 }
+
+-- LSP Hover
+vim.keymap.set("n", "K", function()
+  vim.lsp.buf.hover {
+    border = "rounded",
+  }
+end, { desc = "LSP Hover" })
 -- read :h vim.lsp.config for changing options of lsp servers
