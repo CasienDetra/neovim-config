cd4aa61153ab6cb786e0a5e20cd82efad3098bd1
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-08-10 12:33:54 +0700

    my hsd

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,8 +8,17 @@ map("n", "<C-S-h>", "<C-w><")
 map("n", "<C-S-l>", "<C-w>>")
 map("n", "<C-S-k>", "<C-w>+")
 map("n", "<C-S-j>", "<C-w>-")
+
 map("n", "<leader>fr", "<cmd> Telescope oldfiles <cr>")
 
+-- Do things without affecting the registers
+map("n", "d", '"_d')
+map("n", "D", '"_D')
+map("n", "x", '"_x')
+map("n", "X", '"_X')
+
+map("v", "d", '"_d')
+map("v", "x", '"_x')
 map("n", "-", function()
   vim.cmd "Oil"
 end)
